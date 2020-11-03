55242676efe88e98ffe399cbda6e433fdde04686
Alex Mous <48971842+polarpiberry@users.noreply.github.com>
2020-11-03 15:05:08 -0800

    Add features to chatbot

--- chatbot_train.js
+++ chatbot_train.js
@@ -6,6 +6,9 @@ const manager = new NlpManager({ languages: ['en'], forceNER: true });
 
 //Angry moods
 manager.addDocument('en', 'arrg', 'moods.angry');
+manager.addDocument('en', 'bad bot', 'moods.angry');
+manager.addDocument('en', 'naughty bot', 'moods.angry');
+manager.addDocument('en', 'awful bot', 'moods.angry');
 manager.addDocument('en', 'no don\'t do that', 'moods.angry');
 manager.addDocument('en', 'don\'t', 'moods.angry');
 manager.addDocument('en', 'wasn\'t talking to you', 'moods.angry');
@@ -31,6 +34,29 @@ manager.addDocument('en', 'i have caused wrong', 'moods.remorseful');
 manager.addDocument('en', 'i have caused harm', 'moods.remorseful');
 manager.addDocument('en', 'please forgive me', 'moods.remorseful');
 manager.addDocument('en', 'forgive me for my wrongs', 'moods.remorseful');
+
+//Happy moods
+manager.addDocument('en', 'i\'m happy', 'moods.happy');
+manager.addDocument('en', 'that\'s awesome', 'moods.happy');
+manager.addDocument('en', 'that\'s great', 'moods.happy');
+manager.addDocument('en', 'that\'s wonderful', 'moods.happy');
+manager.addDocument('en', 'that\'s epic', 'moods.happy');
+
+//Complimentary moods
+manager.addDocument('en', 'you\'re awesome', 'moods.compliment');
+manager.addDocument('en', 'you\'re great', 'moods.compliment');
+manager.addDocument('en', 'you\'re good', 'moods.compliment');
+manager.addDocument('en', 'good bot', 'moods.compliment');
+manager.addDocument('en', 'great bot', 'moods.compliment');
+manager.addDocument('en', 'wonderful bot', 'moods.compliment');
+
+//Thank yous
+manager.addDocument('en', 'thank you', 'moods.thanks');
+manager.addDocument('en', 'that really helped', 'moods.thanks');
+manager.addDocument('en', 'thank you you\'re helpful', 'moods.thanks');
+manager.addDocument('en', 'thanks bot', 'moods.thanks');
+manager.addDocument('en', 'thank you bot', 'moods.thanks');
+manager.addDocument('en', 'you\'re really helpful', 'moods.thanks');
 
 //Greetings
 manager.addDocument('en', 'hello', 'greetings.hello');
@@ -63,12 +89,27 @@ manager.addAnswer('en', 'greetings.hello', 'Greetings!');
 
 manager.addAnswer('en', 'moods.angry', 'Oh no, I\'m sorry! :frowning:');
 manager.addAnswer('en', 'moods.angry', 'Sorry! :worried:');
+manager.addAnswer('en', 'moods.angry', 'I\'m sorry! I\'m trying my best...');
 manager.addAnswer('en', 'moods.angry', 'I\'m sorry, I\'m just a lonely bot! :slight_frown:');
 
 manager.addAnswer('en', 'moods.sad', 'It could be worse - you could be an emotionless bot like me!');
 
 manager.addAnswer('en', 'moods.remourseful', 'It\'s okay... I don\' have feelings like you humans');
 manager.addAnswer('en', 'moods.remourseful', 'Cheer up! :heart: from your emotionless bot');
+
+manager.addAnswer('en', 'moods.happy', 'I\'m happy too!');
+manager.addAnswer('en', 'moods.happy', 'I\'m glad!');
+manager.addAnswer('en', 'moods.happy', 'That\'s great!');
+manager.addAnswer('en', 'moods.happy', 'I\'m glad that you\'re happy!');
+
+manager.addAnswer('en', 'moods.compliment', 'Thank you for your kind words! Human kindness knows no bounds');
+manager.addAnswer('en', 'moods.compliment', 'Your kindness makes my silicon glow!');
+manager.addAnswer('en', 'moods.compliment', 'Your kind words make my wires warm!');
+
+manager.addAnswer('en', 'moods.thanks', 'You\'re welcome! It was my pleasure to be of assistance');
+manager.addAnswer('en', 'moods.thanks', 'No problem! I\'m always happy to help a human');
+manager.addAnswer('en', 'moods.thanks', 'You\'re welcome! Human kindness knows no bounds');
+
 
 //Commands
 manager.addAnswer('en', 'commands.joke', '~JOKE');
